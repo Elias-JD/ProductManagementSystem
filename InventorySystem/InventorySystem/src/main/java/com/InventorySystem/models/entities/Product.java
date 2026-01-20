10ababb2f39a0937b853bfdc24835de940462f52
Elias Diaz <Eliasdiazfeliz@gmail.com>
2026-01-19 21:49:37 -0400

    Add product entity and jpa repository.

---
+++ InventorySystem/InventorySystem/src/main/java/com/InventorySystem/models/entities/Product.java
@@ -0,0 +1,40 @@
+package com.InventorySystem.models.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "products")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private Double price;
+    private Integer stock;
+    private String category;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+}
